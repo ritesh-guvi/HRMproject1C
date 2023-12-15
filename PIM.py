e20ba00883f610f55c01a18ae8edd6fd6e29226e
ritesh-guvi <143256013+ritesh-guvi@users.noreply.github.com>
2023-12-15 16:34:20 +0530

    Add files via upload
    Add a new employee in the PIM Module Project

---
+++ PIM.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from time import sleep
+
+
+class Ritesh:
+    def __init__(self, url):
+       self.url = url
+       self.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+       self.username = 'Admin'
+       self.password = 'admin123'
+
+
+    
+    def login(self):
+        self.driver.maximize_window()
+        self.driver.get(self.url)
+        self.driver.implicitly_wait(8)
+        self.driver.find_element(by=By.NAME, value='username').send_keys(self.username)
+        self.driver.find_element(by=By.NAME, value='password').send_keys(self.password)
+
+        sleep(2)
+        self.driver.find_element(by=By.XPATH, value='//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button').click()
+
+        sleep(3)
+        self.driver.find_element(by=By.XPATH, value='//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]').click()                         
+        sleep(3)
+
+        self.driver.find_element(by=By.XPATH, value='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[1]/button/i').click()
+
+        self.driver.find_element(by=By.XPATH, value='/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[1]/div/div/div[2]/div[1]/div[2]/input').click()
+    
+        self.firstName = 'ritesh'
+        self.middleName = 'kumar'
+        self.lastName = 'gupta'
+
+        sleep(4)
+        self.driver.find_element(by=By.NAME, value='firstName').send_keys(self.firstName)
+
+        self.driver.find_element(by=By.NAME, value='middleName').send_keys(self.middleName)
+
+        
+        self.driver.find_element(by=By.NAME, value='lastName').send_keys(self.lastName)
+
+
+
+        self.driver.find_element(by=By.XPATH, value='/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]').click()
+
+
+url = 'https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPimModule'
+
+Ritesh(url).login()
